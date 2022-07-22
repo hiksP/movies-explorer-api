52278bc5702894db8ad2757d2320547e0059e3e7
Peter Plavskikh <qksyo@yandex.ru>
2022-07-22 20:12:19 +0700

    feature: app created and user scheme

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const PORT = 3000;
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log(req.method, req.path);
+    next();
+  });
+
+app.use(express.json());
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1/bitfilmsdb');
+  
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+    });
+  }
+  
+main();
