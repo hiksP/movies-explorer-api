3dee35b6af317d5730bd27cd09d2c08c1e13de5d
Plavskikh Peter <qksyo@yandex.ru>
2022-08-23 17:38:02 +0700

    check

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -19,7 +19,7 @@ module.exports = (req, res, next) => {
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.header('Access-Control-Allow-Headers', 'Authorization', requestHeaders);
     return res.end();
   }
   next();
